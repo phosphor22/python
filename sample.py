89d6e7dbeb9dcecc1c1b267ea9afc78b90518ffd
phosphor22 <36332128+phosphor22@users.noreply.github.com>
2018-02-10 22:55:53 +0900

    Add files via upload

--- sample.py
+++ sample.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+from binance.client import Client
+
+#2018/1/9 修正
+api_key = "***"
+api_secret = "***"
+
+client = Client(api_key, api_secret)
+
+
+
+# 全銘柄取得
+ticker = client.get_ticker(symbol="XRPBTC")
+
+ask = ticker['askPrice']
+bid = ticker['bidPrice']
+        
+print("■binance：" + "XRPBTC")
+print('bid:'+ str(bid))
+print('ask:'+ str(ask))
